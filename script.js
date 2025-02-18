7de741db780a9e4e1014c0f66cf49d9088008434
dolsotbob <1hyoon3@naver.com>
2025-02-18 18:20:15 +0900

    modified

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ function calculate(n1, operator, n2) {
   let result = 0;
   // TODO : n1과 n2를 operator에 따라 계산하는 함수를 만드세요.
   // ex) 입력값이 n1 : '1', operator : '+', n2 : '2' 인 경우, 3이 리턴됩니다.
+  switch (operator) {
+    case '+':
+      result = Number(n1) + Number(n2);
+      break;
+    case '-':
+      result = Number(n1) - Number(n2);
+      break;
+    case '*':
+      result = Number(n1) * Number(n2);
+      break;
+    case '/':
+      result = Number(n1) / Number(n2);
+      break;
+  }
   return String(result);
 }
 
@@ -27,23 +41,35 @@ buttons.addEventListener('click', function (event) {
     if (action === 'number') {
       // 그리고 버튼의 클레스가 number이면
       // 아래 코드가 작동됩니다.
-      console.log('숫자 ' + buttonContent + ' 버튼');
+      if (firstOperend.textContent === '0') {
+        firstOperend.textContent = buttonContent;
+      } else {
+        secondOperend.textContent = buttonContent;
+      }
     }
 
     if (action === 'operator') {
-      console.log('연산자 ' + buttonContent + ' 버튼');
+      operator.textContent = buttonContent;
     }
 
     if (action === 'decimal') {
-      // console.log('소수점 버튼');
+      console.log('소수점 버튼');
     }
 
     if (action === 'clear') {
       console.log('초기화 버튼');
+      firstOperend.textContent = '0';
+      secondOperend.textContent = '0';
+      operator.textContent = '+';
+      calculatedResult.textContent = '0';
     }
 
     if (action === 'calculate') {
-      console.log('계산 버튼');
+      calculatedResult.textContent = calculate(
+        firstOperend.textContent,
+        operator.textContent, 
+        secondOperend.textContent
+      );
     }
   }
 });
